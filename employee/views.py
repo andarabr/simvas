b0182f62c764b80d76af6d8fc997de8316c6e499
andarabr <andarabrey@gmail.com>
2021-10-28 15:40:35 +0700

    add basic routing to login, user, report, management

--- employee/views.py
+++ employee/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.shortcuts import redirect, render
+from .models import User
+from pprint import pprint
 
 # Create your views here.
 def index(request):
     if request.user.is_authenticated:
-        return render(request, "employee/dashboard.html")
+        if request.user.is_staff:
+            users = User.objects.select_related('employee').order_by('id');
+            pprint(users)
+            context = {'users' : users}
+            return render(request, "employee/dashboard.html", context)
+        else:
+            return redirect('index-report')
     else:
         return redirect('/accounts/login/')
+
+def reset_pass(id):
+    return "test"
