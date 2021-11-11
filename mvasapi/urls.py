3fd5ed9d87c16f229300c4b9ff5db40f5fa1b090
andarabr <andarabrey@gmail.com>
2021-11-11 14:55:13 +0700

    Add Historical Data Page & Polish Report Table UI

--- mvasapi/urls.py
+++ mvasapi/urls.py
@@ -4,7 +4,8 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('reports/', views.index, name='index-report'),
+    path('reports-history/', views.history, name='history-report'),
     # path('about/', views.about, name='blog-about'),
     path('api-test/', views.api_test, name='api-test'),
-    path('api-test/<int:id>', views.api_test_idv, name='api-test-idv'),
+    path('api-test/<int:id>/<str:username>', views.api_test_idv, name='api-test-idv'),
 ]
--- mvasapi/views.py
+++ mvasapi/views.py
@@ -5,16 +5,39 @@ import requests
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.core import serializers
+from datetime import datetime
+from django.core.paginator import Paginator
+
 
 # Create your views here.
 def index(request):
     if request.user.is_authenticated:
-        transactions = Transaction.objects.all().order_by('id');
-        context = {'transactions' : transactions}
-        return render(request, "mvasapi/index.html", context)
+        transactions = Transaction.objects.all().exclude(http_status__in=[201, 200]).order_by('id');
+        paginator = Paginator(transactions, 10) # Show 25 contacts per page.
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        killpunctuation = str.maketrans('', '', r"!#$%^&*'()?/;<>")
+        text = "B!#$%^&*'()?/;<>reygas"
+        text = text.translate(killpunctuation)
+
+        # context = {'transactions' : transactions}
+        return render(request, "mvasapi/index.html", {'page_obj': page_obj})
     else:
         return redirect('/accounts/login/')
 
+def history(request):
+    if request.user.is_authenticated:
+        transactions = Transaction.objects.all().filter(http_status__in=[201, 200]).order_by('id');
+        paginator = Paginator(transactions, 10) # Show 25 contacts per page.
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        # context = {'transactions' : transactions}
+        return render(request, "mvasapi/history.html", {'page_obj': page_obj})
+    else:
+        return redirect('/accounts/login/')
 
 def api_test(request):
     payload = {'key1': 'value1', 'key2': 'value2'}
@@ -23,8 +46,8 @@ def api_test(request):
     print(postdata)
     return JsonResponse(postdata)
 
-def api_test_idv(request, id):
-    # print(id)
+def api_test_idv(request, id, username):
+    print(username)
     transactions = get_object_or_404(Transaction, id=id);
     if transactions.far_rate == 0:
         transactions.far_rate = ''
@@ -62,11 +85,21 @@ def api_test_idv(request, id):
     response = requests.post('https://api.bi.go.id/bi/sismontavar/sismontavar/data', data=data, headers=headers)
     # response = requests.post('https://reqres.in/api/users', json=data, headers=headers)
     postdata = response.json()
-    print(response.status_code)
-    # return JsonResponse(postdata)
+    print(response.reason)
+
+    if response.status_code == 201 or 400:
+        if transactions.far_rate == '':
+            transactions.far_rate = 0
+        transactions.http_status = response.status_code
+        transactions.http_status_desc = response.reason
+        transactions.last_post_try = datetime.now()
+        transactions.last_posted_by = username
+        transactions.save()
+
+    return JsonResponse(postdata)
     # return JsonResponse(postdata['httpCode'] ,safe=False)
     # return JsonResponse(dict(request.headers), safe=False)
-    return JsonResponse(response.status_code, safe=False)
+    # return JsonResponse(response.status_code, safe=False)
 
 
 
